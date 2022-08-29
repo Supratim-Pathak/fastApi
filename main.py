1d63bf8c26a179b4540dcc13d4bb05a0a026e401
Supratim-Pathak <supratimpathak@gmail.com>
2022-08-29 23:26:07 +0530

    ------

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return "ok"
+    return {'data':{'name':'supratim'}}
+
+@app.get("/about/{id}")
+def about(id:int):
+    # return {'data':{'about':'testing Aboutpage {id}' }}
+    return {'data':{id, 2 }}
